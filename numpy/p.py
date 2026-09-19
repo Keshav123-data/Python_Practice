71c19cf3e2194db7337c346ba62425738793417e
Keshav123-data <keshavshinde9881@gmail.com>
2026-09-19 18:40:56 +0530

    Added more prctice in numpy

--- numpy/p.py
+++ numpy/p.py
@@ -119,3 +119,85 @@ print("array:",arra)
 
 filtered = arra[(arra > 5) & (arra < 16)]
 print("filtered array:",filtered)
+
+# Q. Create a NumPy array containing numbers from 10 to 100.
+
+numpy_array = np.array(range(20,100))
+print("\nnumpy array:",numpy_array)
+
+# Q. Find the mean of all values greater than 50.
+
+values = numpy_array[numpy_array > 50]
+mean3 = np.mean(values)
+
+print("\nmean of numbers which are greater than 50:",mean3)
+
+# Q. Replace all negative values in an array with 0.
+
+array5 = np.array([1,-3,4,-7,45,-4,635,5,-5,635,-35,7,76,-4,6,6,66,-46,6,67,6])
+
+array5[array5 < 0 ] = 0
+
+print("\nnew array:",array5)
+
+# Q. Find the number of values that are: Greater than 50. Less than 20, Between 20 and 50.
+
+print("\nnumbers are greter than 50 :",np.sum(array5 > 50))
+
+print("numbers are less than 20:",np.sum(array5 < 20))
+
+print("numbers are between 20 and 50:",np.sum((array5 > 20) & (array5 < 50)))
+
+# Q. Create a 5×5 matrix and calculate the sum of its diagonal.
+
+matrix = np.array(np.arange(25).reshape(5,5))
+print("matrix:",matrix)
+
+sum_diagonal = np.trace(matrix)
+# also
+sum_diag = np.sum(np.diag(matrix))
+
+print("\nsum of diagonal:",sum_diagonal,"also", sum_diag)
+
+# Q. Find the maximum value from each row of a NumPy matrix.
+
+max_values = np.max(matrix, axis = 1)
+print("\nmax of eaxh row:",max_values)
+
+# Q. Find the minimum value from each column.
+
+min_values = np.min(matrix, axis = 0)
+print("\nmin value of each column:",min_values)
+
+# Q. Create an array of 20 random integers between 1 and 100 and find:Mean, Median, Standard deviation, Maximum, Minimum.
+
+array6 = np.random.randint(1,101,20)
+print("array",array6)
+
+mean_array = np.mean(array6)
+median_array = np.median(array6)
+std_array = np.std(array6)
+max_array = np.max(array6)
+min_array = np.min(array6)
+
+print("\nmean: ",mean_array)
+print("median:",median_array)
+print("standerd deviation:",std_array)
+print("maximum:",max_array)
+print("minimum:",min_array)
+
+# Q .Normalize this data:
+
+data = np.array([10, 20, 30, 40, 50])
+
+normalized_data = (data - np.min(data)) / (np.max(data) - np.min(data))
+print("\nnormalized data:",normalized_data)
+
+# Q. Find the unique values and their frequency from a NumPy array.
+unique_array = np.array([20,30,20,40,30,10,30,50,60,70,80,90,101,202,404,404,505,808,202,3,45,505])
+print("\narray:",unique_array)
+
+unique_values, frequency = np.unique(unique_array, return_counts = True)
+
+print("\nunique values:",unique_values)
+print("frequency:", frequency)
