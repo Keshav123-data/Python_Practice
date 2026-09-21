5472f9ef2788f6d0d93267d93353b878c549983b
Keshav123-data <keshavshinde9881@gmail.com>
2026-09-22 00:54:46 +0530

    added more practice

--- analytics/p.py
+++ analytics/p.py
@@ -67,3 +67,73 @@ summary = df.groupby("product").agg(
 )
 
 print("\nsummary:", summary)
+
+
+data = {
+    "Product": ["Laptop", "Mouse", "Keyboard", "Laptop", "Mouse",
+                "Monitor", "Keyboard", "Laptop"],
+    "Region": ["North", "South", "East", "West", "North",
+               "South", "East", "North"],
+    "Sales": [55000, 1200, 2500, 60000, 1500, 18000, 3000, 58000],
+    "Quantity": [2, 5, 3, 2, 6, 2, 4, 2]
+}
+
+DataFrame = pd.DataFrame(data)
+print("\nDAtaFrame", DataFrame)
+
+# Q. Calculate total sales.
+print("\nTotal Sales:\n",DataFrame["Sales"].sum())
+
+# Q. Calculate average sales.
+print("\nAverage Sales:\n", DataFrame["Sales"].mean())
+
+# Q. Find the highest sales transaction.
+print("\nHighest Sales Transaction:\n",DataFrame["Sales"].max())
+
+# Q. Find the lowest sales transaction.
+print("\nLowest Sales Transaction:\n", DataFrame["Sales"].min())
+
+# Q. Find total quantity sold.
+
+print("\nTotal Quantity:\n",DataFrame["Quantity"].sum())
+
+# Q. Find total sales by product.
+
+print("\nTotal Sales by Products:\n", DataFrame.groupby("Product")["Sales"].sum())
+
+# Q. Find total sales by region.
+
+print("\nTotal Sales by Region:\n", DataFrame.groupby("Region")["Sales"].sum())
+
+# Q. Find average sales by region.
+
+print("\nAverage Sales by Region:\n", DataFrame.groupby("Region")["Sales"].mean())
+
+# Q. Find the product with the highest total sales.
+print("\nProduct with Highest Total Sales:\n",(DataFrame.groupby("Product")["Sales"].sum()).idxmax())
+
+# Q. Find the region with the highest total sales.
+
+print("\nRegion with highest Total Sales:\n", (DataFrame.groupby("Region")["Sales"].sum()).sort_values(ascending=False).head(1))
+
+# Q. Find total quantity sold for each product.
+print("\nTotal Quantity by Prouduct:\n", DataFrame.groupby("Product")["Quantity"].sum())
+
+# Q. Add a column: Sales_Per_Unit = Sales / Quantity
+
+DataFrame["Sales_Per_Unit"] = DataFrame["Sales"] / DataFrame["Quantity"]
+
+print("\nNew DataFrame:\n", DataFrame)
+
+# Q. Find the product with the highest sales per unit.
+
+pr_max_per_unit = DataFrame.groupby("Product")["Sales_Per_Unit"].max()
+print("\nProduct with Highest Sales Per Unit:\n", pr_max_per_unit )
+
+# Q. Find the top 3 sales transactions.
+print("\nTop 3 Sales Transactions:\n", DataFrame.sort_values("Sales", ascending=False).head(3))
+
+# Q. Find all transactions where sales are greater than ₹20,000.
+
+print("\nSales which are greater than 20000:\n", DataFrame[DataFrame["Sales"] > 20000])
+
